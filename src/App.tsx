cd1e6c9d19646ad61d7d68297308b43e71c133a9
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-04-06 10:35:55 -0400

    Add Title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
 import './App.css';
 
+function Title(props: { text: string }) {
+
+    return (<h1>
+        {props.text}
+    </h1>);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Title text={'Recipes'} />
+        </div>
+    );
 }
 
 export default App;
