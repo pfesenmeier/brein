c48565bc1de5e4d2f780f1fd1b0e2e76ed2462e6
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-04-06 14:50:03 -0400

    add mui, basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import './App.css';
+import { Title } from './Title';
+import Button from '@mui/material/Button';
 
-function Title(props: { text: string }) {
+function Recipe(props: { name: string }) {
+    return (<li>{props.name}</li>);
+}
 
-    return (<h1>
-        {props.text}
-    </h1>);
+function RecipeList(props: { recipes: { name: string }[] }) {
+    return (<ul>
+        {props.recipes.map(recipe => <Recipe name={recipe.name} />)}
+    </ul>);
 }
 
 function App() {
+    const recipes = [{
+        name: 'foo',
+    }, {
+        name: 'bar',
+    }];
     return (
         <div className="App">
             <Title text={'Recipes'} />
-        </div>
+            <RecipeList recipes={recipes} />
+            <Button>Click me</Button>
+       </div>
     );
 }
 
 export default App;
+
